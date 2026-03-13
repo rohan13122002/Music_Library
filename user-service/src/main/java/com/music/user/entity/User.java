05e820e09924a89b641a9e09e18e5e716387ff40
Rohan Desai <159284017+rohan13122002@users.noreply.github.com>
2026-03-13 15:05:30 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/music/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.music.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+//creates a table with name users
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotBlank
+	private String name;
+	
+	@Column(unique=true)
+	private String email;
+	
+	@NotBlank
+	private String password;
+	private String phone;
+
+}
